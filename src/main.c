48d4ee6a8d13cd9a9dad1d0254fca1045f2d7d71
seonghyk <seonghyk@student.42seoul.kr>
2022-10-07 16:46:57 +0900

    f

--- src/main.c
+++ src/main.c
@@ -27,26 +27,26 @@ t_ord_desc	*nums_to_ord_int_stack(
 	int				i;
 	int				id;
 	t_ord_desc		*descs;
-	
+	enum e_order	o_orign;
+	enum e_order	o_crnt;
+
 	descs = (t_ord_desc *)malloc(sizeof(t_ord_desc) * (nums->len / 2 + 1));
 	id = 0;
 	i = -1;
 	while (++i < nums->len - 1)
 	{
-		enum e_order	o_orign = compare(nums->arr[i], nums->arr[i + 1]);
+		o_orign = compare(nums->arr[i], nums->arr[i + 1]);
 		++id;
 		descs[id].id = id;
 		descs[id].size = 1;
 		descs[id].ord = o_orign;
 		while (i < nums->len)
 		{
-			enum e_order o_crnt = compare(nums->arr[i], nums->arr[i + 1]);
+			o_crnt = compare(nums->arr[i], nums->arr[i + 1]);
 			ft_deque_typesymbol_push_back(
 				stack,
-				(_typesymbol){
-					nums->arr[i],
-					&descs[id]
-				});
+				(_typesymbol){nums->arr[i], &descs[id]}
+				);
 			descs[id].size++;
 			if (o_orign != o_crnt)
 				break ;
@@ -56,13 +56,14 @@ t_ord_desc	*nums_to_ord_int_stack(
 	return (descs);
 }
 
-
 int	main(int ac, char *av[])
 {
 	int					i;
 	t_parsed_num		numbers;
 	t_deque_typesymbol	a_stack;
 	t_deque_typesymbol	b_stack;
+	t_ord_int			n; 
+	t_ord_desc			*descs;
 
 	if (ac < 1)
 		return (0);
@@ -70,12 +71,11 @@ int	main(int ac, char *av[])
 	a_stack = ft_deque_typesymbol_create(numbers.len + 1);
 	b_stack = ft_deque_typesymbol_create(numbers.len + 1);
 	i = 0;
-	int prev = numbers.arr[i];
-	t_ord_desc	*descs = nums_to_ord_int_stack(&numbers, &a_stack);
+	descs = nums_to_ord_int_stack(&numbers, &a_stack);
 	i = -1;
 	while (++i < numbers.len)
 	{
-		t_ord_int	n = ft_deque_typesymbol_pop_front(&a_stack);
+		n = ft_deque_typesymbol_pop_front(&a_stack);
 		printf("%d %d\n", n.value, n.ord->id);
 	}
 }
